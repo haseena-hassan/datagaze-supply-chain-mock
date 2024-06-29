2b214c9c4e5f386fa4816971ad75b5717444f1cc
haseena-hassan <haseena2199@gmail.com>
2024-06-29 15:25:40 +0530

    feat: Implemented basic game functionality with dummy players

--- src/main/App.java
+++ src/main/App.java
@@ -1,11 +1,24 @@
 package src.main;
 
+import src.main.model.Arena;
+import src.main.model.Player;
 
 public class App {
     public static void main(String[] args) {
 
+        Player player1 = new Player("Alice", 50, 10, 8);
+        Player player2 = new Player("Bob", 100, 5, 12);
+
+        Arena arena = new Arena(player1, player2);
         System.out.println("Starting the Magical Arena Game!");
-        // TODO: Create players
-        // TODO: Start game and print winner when game ends
+
+        System.out.println("Player 1: " + player1.toString());
+        System.out.println("Player 2: " + player2.toString());
+
+        arena.fight();
+        if(arena.isGameOver()) {
+            String winner = (player1.getHealth() <= 0) ? player2.getName() : player1.getName();
+            System.out.println("Game Over! " + winner + " wins!");
+        }
     }
 }
