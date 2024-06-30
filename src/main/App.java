459a7675d50eaadcebb1c2d1fa8b1b580cd53145
haseena-hassan <haseena2199@gmail.com>
2024-06-30 11:08:10 +0530

    feat: create players using user input attributes from command-line

--- src/main/App.java
+++ src/main/App.java
@@ -4,17 +4,34 @@ import src.main.model.Arena;
 import src.main.model.Dice;
 import src.main.model.Player;
 
+import java.util.Scanner;
+
 public class App {
+
+    // TODO: implement dynamic player creation based on user choice
+
+    public static Player createPlayer() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("\nEnter name of the player: ");
+        String name = scanner.next();
+        System.out.print("Enter health for " + name + " (give positive integer): ");
+        int health = scanner.nextInt();
+        System.out.print("Enter strength for " + name + " (give positive integer): ");
+        int strength = scanner.nextInt();
+        System.out.print("Enter attack for " + name + " (give positive integer): ");
+        int attack = scanner.nextInt();
+        return new Player(name, health, strength, attack);
+    }
+
     public static void main(String[] args) {
 
-        // TODO: User to be able to input the player information
-
-        Player player1 = new Player("Alice", 40, 2, 6);
-        Player player2 = new Player("Bob", 50, 4, 4);
+        Player player1 = createPlayer();
+        Player player2 = createPlayer();
         Dice dice = new Dice(6);
 
         Arena arena = new Arena(player1, player2, dice);
-        System.out.println("Starting the Magical Arena Game!");
+        System.out.println("\nStarting the Magical Arena Game!");
 
         System.out.println("Player 1: " + player1);
         System.out.println("Player 2: " + player2);
